37690b1a26b1e67ac54e4a064a8a83b180cbf66a
pockyes <tele1841@gmail.com>
2021-06-04 17:43:50 +0900

    #1.0 Creating your first React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) {
+  return <div>
+    <h2>{name} is good</h2>
+    <img src={picture} />
+    </div>
+}
+
+let foodLike = [
+  {
+  name: "kimchi",
+  Image: "https://img.kr.news.samsung.com/kr/wp-content/uploads/2017/12/2-11.png"
+},
+{
+  name: "ramen",
+  Image: "https://i.pinimg.com/736x/1d/74/7b/1d747b37131d89619b4899533eac0f7b.jpg"
+}, 
+{
+  name: "rice",
+  Image: "https://image.shutterstock.com/image-vector/bowl-rice-260nw-416257243.jpg"
+}, 
+{
+  name: "salad",
+  Image: "https://img.pikbest.com/png-images/qiantu/hand-drawn-original-cartoon-food-western-food-vegetable-salad_2580353.png!bw800"
+}, 
+{
+  name: "potato",
+  Image: "https://pds.joins.com/news/component/htmlphoto_mmdata/201412/11/htm_2014121113444320102011.jpg"
+}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodLike.map(dish => (
+      <Food name={dish.name} picture={dish.Image} />
+    ))}
+  </div>
   );
 }
 
