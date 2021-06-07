583560d09859107ddf1bf239f65fe49567fa5f6c
pockyes <tele1841@gmail.com>
2021-06-07 16:56:09 +0900

    버튼을 생성해 state가 증가하고 감소하는 이벤트 추가 (210607)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({name, picture}) {
-  return <div>
-    <h2>{name} is good</h2>
-    <img src={picture} />
-    </div>
-}
-
-let foodLike = [
-  {
-  name: "kimchi",
-  Image: "https://img.kr.news.samsung.com/kr/wp-content/uploads/2017/12/2-11.png"
-},
-{
-  name: "ramen",
-  Image: "https://i.pinimg.com/736x/1d/74/7b/1d747b37131d89619b4899533eac0f7b.jpg"
-}, 
-{
-  name: "rice",
-  Image: "https://image.shutterstock.com/image-vector/bowl-rice-260nw-416257243.jpg"
-}, 
-{
-  name: "salad",
-  Image: "https://img.pikbest.com/png-images/qiantu/hand-drawn-original-cartoon-food-western-food-vegetable-salad_2580353.png!bw800"
-}, 
-{
-  name: "potato",
-  Image: "https://pds.joins.com/news/component/htmlphoto_mmdata/201412/11/htm_2014121113444320102011.jpg"
-}
-];
-
-function App() {
-  return (
-  <div>
-    {foodLike.map(dish => (
-      <Food name={dish.name} picture={dish.Image} />
-    ))}
-  </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render () {
+    return (
+      <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
