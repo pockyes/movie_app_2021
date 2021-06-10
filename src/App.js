3aae2be089675436f3561a2602be6cc35d52bc5e
pockyes <tele1841@gmail.com>
2021-06-10 14:46:31 +0900

    Getting Ready for the Router  (210610)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from './Movies';
+import Movie from './components/Movies';
 import "./App.css";
 
 class App extends React.Component{
